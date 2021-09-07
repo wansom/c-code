2027bdacb3a35987a24eca0519a2ace3c8510d34
wansom <ochiengwarren10@gmail.com>
2021-09-07 22:22:47 +0300

    object functions

--- main.cpp
+++ main.cpp
@@ -10,20 +10,29 @@ public:
     string name;
     string location;
     int age;
-    User(string aname, string alocation, int cage){
+    string role;
+    User(string aname, string alocation, int cage, string myrole){
         name = aname;
         location = alocation;
         age = cage;
+        role = myrole;
     };
+    bool isAdmin(){
+        if(role =="admin"){
+            return true;
+        }
+    return false;
+    }
 };
 
 int main()
 {
     sayHi();
     cout << "Hello world!" << endl;
-    User voter("warren","kisumu", 24);
+    User voter("warren","kisumu", 24,"admin");
 
-    cout <<voter.name;
+    cout <<voter.age;
+    cout << voter.isAdmin();
 
     return 0;
 }
